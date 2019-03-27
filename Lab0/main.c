cb1a9c11fbdf5c12a2133187d2158dc6a7f40941
Dan <doates625@gmail.com>
2019-03-27 11:03:50 -0400

    Added Lab 0 submission

--- Lab0/main.c
+++ Lab0/main.c
@@ -1,7 +1,7 @@
 /**
  * main.c
  *
- * ECE-3849 Lab 1 code submission
+ * ECE-3849 Lab 0 code submission
  * Submitted XX-XX-2019
  *
  * Hardware:
@@ -14,22 +14,22 @@
  */
 
 // Dependencies
-#include "Globals.h"
-#include "Adc.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include "driverlib/fpu.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/interrupt.h"
+#include "buttons.h"
 #include "Crystalfontz128x128_ST7735.h"
 #include <stdio.h>
 
 // Global Variables
+uint32_t gSystemClock;          // System clock frequency [Hz]
+volatile uint32_t gTime = 0;    // Time [units of 0.01s]
 
 // Main Function
 int main(void)
 {
-    // Disable interrupts during configuration
     IntMasterDisable();
 
     // Enable FPU and permit ISRs to use it
@@ -49,20 +49,54 @@ int main(void)
     GrContextFontSet(&sContext, &g_sFontFixed6x8);
 
     // Initialize buttons
-    // ButtonInit(); // NO BUTTONS YET***
+    ButtonInit();
     IntMasterEnable();
 
     // Local variables
+    uint32_t time;      // Copy of gTime
+    uint32_t buttons;   // Copy of gButtons
+    char str_time[50];  // String buffer for time
+    char str_butt[50];  // String buffer for buttons
     tRectangle rectFullScreen = {0, 0, GrContextDpyWidthGet(&sContext)-1, GrContextDpyHeightGet(&sContext)-1};
 
     // Graphics loop
-    while (1)
+    while (true)
     {
         // Fill screen with black
         GrContextForegroundSet(&sContext, ClrBlack);
         GrRectFill(&sContext, &rectFullScreen);
 
-        // Write more things to LCD...
+        // Get time and convert to string
+        time = gTime;
+        uint8_t ff = time % 100;
+        uint8_t ss = (time / 100) % 60;
+        uint8_t mm = (time / 6000);
+        snprintf(str_time, sizeof(str_time), "Time = %02u:%02u:%02u", mm, ss, ff);
+
+        // Print time string as yellow text to LCD
+        GrContextForegroundSet(&sContext, ClrYellow);
+        GrStringDraw(&sContext, str_time, /*length*/ -1, /*x*/ 0, /*y*/ 0, /*opaque*/ false);
+
+        // Display gButtons bitmap
+        buttons = gButtons;
+        // snprintf(str_butt, sizeof(str_butt), "Buttons = %04u", buttons);
+        snprintf(str_butt, sizeof(str_butt), "Buttons = ");
+        uint8_t i;
+        for(i = 0; i < 9; i++)
+        {
+            if(buttons & (1 << i))
+            {
+                str_butt[18 - i] = '1';
+            }
+            else
+            {
+                str_butt[18 - i] = '0';
+            }
+        }
+
+        // Print buttons bitmap as yellow text to LCD
+        GrContextForegroundSet(&sContext, ClrYellow);
+        GrStringDraw(&sContext, str_butt, /*length*/ -1, /*x*/ 0, /*y*/ 20, /*opaque*/ false);
 
         // Flush LCD frame buffer
         GrFlush(&sContext);
